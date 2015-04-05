627a5e18acdce3be32c4f4f6604adc0adab3f254
jpfulton <jpatrick.fulton@gmail.com>
2015-04-05 15:06:36 -0400

    Bundles implementation.

---
+++ MartyJohnBonales.Web/App_Start/BundlesConfig.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace MartyJohnBonales.Web
+{
+    public class BundlesConfig
+    {
+
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            BundleTable.EnableOptimizations = true;
+            bundles.UseCdn = true;
+
+            var jqueryCdn = "//ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js";
+            bundles.Add(new ScriptBundle("~/bundles/jqueryjs", jqueryCdn)
+                .Include("~/Scripts/jquery-{version}.js"));
+
+            var bootstrapCdn = "//netdna.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js";
+            bundles.Add(new ScriptBundle("~/bundles/bootstrapjs", bootstrapCdn)
+                .Include("~/Scripts/bootstrap.js"));
+
+            var bootstrapCssCnd = "//netdna.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css";
+            var boostrapCssBundle = new StyleBundle("~/bundles/bootstrapcss", bootstrapCssCnd)
+                .Include("~/Content/bootstrap.css");
+            bundles.Add(boostrapCssBundle);
+
+            var globalCssBundle = new StyleBundle("~/bundles/globalcss")
+                .Include("~/Content/global.css");
+            bundles.Add(globalCssBundle);
+        }
+    }
+}
--- MartyJohnBonales.Web/Global.asax.cs
+++ MartyJohnBonales.Web/Global.asax.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
 
 namespace MartyJohnBonales.Web
@@ -12,6 +13,8 @@ namespace MartyJohnBonales.Web
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
+
+            BundlesConfig.RegisterBundles(BundleTable.Bundles);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
         }
     }
