f05a6e4f7aa55062ca335caf8eac685d9a2e5c1e
jpfulton <jpatrick.fulton@gmail.com>
2015-04-06 13:49:11 -0400

    Removing unneeded code from navbar js.

--- MartyJohnBonales.Web/Scripts/navbar.js
+++ MartyJohnBonales.Web/Scripts/navbar.js
@@ -1,10 +1,7 @@
 ﻿$(function () {
     var currentUrl = document.URL;
 
-    if (currentUrl.indexOf("Index") > -1) {
-        $("#home-navbar-link").addClass("active");
-    }
-    else if (currentUrl.indexOf("Resources") > -1) {
+    if (currentUrl.indexOf("Resources") > -1) {
         $("#resources-navbar-link").addClass("active");
     }
     else if (currentUrl.indexOf("Contact") > -1) {
